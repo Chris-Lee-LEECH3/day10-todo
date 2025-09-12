ec16bac56eb425f69068ab32e3356248ecedf484
LEECH3 <chris.lee@oocl.com>
2025-09-12 14:40:30 +0800

    refactor: recreate project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,8 @@
 import "./App.css";
-import { useState } from "react";
-
-const Counter = () => {
-  const [count, setCount] = useState(0);
-  
-  const add = () => {
-    setCount(prev => prev + 1);
-  }
-
-  const minus = () => {
-    setCount(prev => prev - 1);
-  }
-
-  return (
-    <div>
-      <button onClick={add}>+</button>
-      {count}
-      <button onClick={minus}>-</button>
-    </div>
-  )
-}
 
 function App() {
   return (
     <div className="App">
-      <Counter />
     </div>
   );
 }
