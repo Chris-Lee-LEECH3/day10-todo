5b6ca8994cbc82bcb03b988280c28a78a40ab7f4
LEECH3 <chris.lee@oocl.com>
2025-09-12 14:45:50 +0800

    feat: update for the ToDoGroup, ToItem

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,44 @@
+import { createContext, useContext, useReducer } from "react";
 import "./App.css";
 
+export const initState = [
+  { id: 1, text: "the first todo", done: false },
+  { id: 2, text: "the second todo", done: false },
+];
+
+export const TodoContext = createContext();
+
+const TodoGroup = () => {
+  const { state, dispatch } = useContext(TodoContext);
+  return (
+    <div>
+      {state.map((todo, index) => (
+        <TodoItem key={todo.id} todo={todo} index={index} />
+      ))}
+    </div>
+  );
+}
+
+const TodoItem = ({ todo }) => {
+  return (
+    <div>
+      {todo.text}
+    </div>
+  );
+}
+
+export const todoReducer = (state, action) => {
+  return state;
+};
+
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, initState);
+
   return (
     <div className="App">
+      <TodoContext.Provider value={{ state, dispatch }}>
+        <TodoGroup />
+      </TodoContext.Provider>
     </div>
   );
 }
