d56c5cc66914b9f5e2c1e34997fd2b053561bc76
LEECH3 <chris.lee@oocl.com>
2025-09-12 15:27:45 +0800

    feat: update css for todo-list

--- my-app/src/components/TodoGroup.js
+++ my-app/src/components/TodoGroup.js
@@ -2,14 +2,24 @@ import { useContext } from "react";
 import { TodoContext } from "../contexts/TodoContext";
 import { TodoItem } from "./TodoItem";
 import TodoGenerator from "./TodoGenerator";
+import './../App.css';
 
 export const TodoGroup = () => {
   const { state } = useContext(TodoContext);
+
+  const ToDoList = () => {
+    return (
+      <div className="todo-list">
+        {state.map((todo, index) => (
+          <TodoItem key={todo.id} todo={todo} index={index} />
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div>
-      {state.map((todo, index) => (
-        <TodoItem key={todo.id} todo={todo} index={index} />
-      ))}
+      <ToDoList />
       <TodoGenerator />
     </div>
   );
