72211ef1585e178eed766fc2db132446c4fa7e8a
LEECH3 <chris.lee@oocl.com>
2025-09-15 14:48:18 +0800

    feat: update put, delete for todoItem

--- my-app/src/components/TodoItem.js
+++ my-app/src/components/TodoItem.js
@@ -2,7 +2,13 @@ import { useContext } from "react";
 import { TodoContext } from "../contexts/TodoContext";
 import { useLocation, useNavigate } from "react-router";
 
-export const TodoItem = ({todo, displayDetailBtn = true, displayDeleteBtn = true }) => {
+import api from "../api/mockApi";
+
+export const TodoItem = ({
+  todo,
+  displayDetailBtn = true,
+  displayDeleteBtn = true,
+}) => {
   const { dispatch } = useContext(TodoContext);
   const navigate = useNavigate();
   const location = useLocation();
@@ -12,11 +18,24 @@ export const TodoItem = ({todo, displayDetailBtn = true, displayDeleteBtn = true
     if (isTodoDetailPage) {
       return;
     }
-    dispatch({ type: "TOGGLE_TODO", payload: { id: todo.id } });
+
+    api.put("/todos/" + todo.id, { ...todo, done: !todo.done })
+      .then(() => {
+        dispatch({ type: "TOGGLE_TODO", payload: { id: todo.id } });
+      })
+      .catch((error) => {
+        console.error("Error updating todo:", error);
+      });
   };
 
   const removeTodo = () => {
-    dispatch({ type: "REMOVE_TODO", payload: { id: todo.id } });
+    api.delete("/todos/" + todo.id)
+      .then(() => {
+        dispatch({ type: "REMOVE_TODO", payload: { id: todo.id } });
+      })
+      .catch((error) => {
+        console.error("Error deleting todo:", error);
+      });
   };
 
   const navigateToDone = () => {
